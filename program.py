0d88eef877d9cdb67494c68b6c425824ba962cc8
Pradhan-Arundhati <101728603+Pradhan-Arundhati@users.noreply.github.com>
2023-08-05 19:22:27 +0530

    Add files via upload

---
+++ program.py
@@ -0,0 +1,7 @@
+print("slice email in python")
+email = input("Enter Your Email: ").strip()
+username = email[:email.index("@")]
+domain_name = email[email.index("@")+1:]
+format_ = (f"Your user name is '{username}' and your domain is '{domain_name}'")
+print(format_)
+print("done when it will run")
